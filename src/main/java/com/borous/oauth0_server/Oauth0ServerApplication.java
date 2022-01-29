59afb0e948e2da3f0d3b4fb7cb3dd2b8ff8afa0b
AlexBorous <42853535+AlexBorous@users.noreply.github.com>
2022-01-29 18:51:50 +0200

    chore: init project

---
+++ src/main/java/com/borous/oauth0_server/Oauth0ServerApplication.java
@@ -0,0 +1,13 @@
+package com.borous.oauth0_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Oauth0ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth0ServerApplication.class, args);
+    }
+}
---
+++ src/test/java/com/borous/oauth0_server/Oauth0ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.borous.oauth0_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth0ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
